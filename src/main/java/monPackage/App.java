649912658783c1d6f59fa02492469460c13da04f
gaber <jaafar.gaber@gmail.com>
2021-03-16 00:02:58 +0100

    commit local src

---
+++ src/main/java/monPackage/App.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package monPackage;
+
+/**
+ * @author gaber
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Bonjour EPSI BDX...!");
+
+	}
+
+}
